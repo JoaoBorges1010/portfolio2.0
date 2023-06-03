83880d94d17fb4423c6f95a60bc2d5d7320d4e73
joaoborges <joaoborgesbiz@gmail.com>
2023-06-03 23:58:30 +0100

    added a Promise to contact api to work with production

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   console.log(req.body);
   require("dotenv").config();
   let nodemailer = require("nodemailer");
@@ -20,10 +20,16 @@ export default function handler(req, res) {
     text: req.body.name + req.body.message + " | sent from:" + req.body.email,
     html: `<div>${req.body.message}</div><p>Sent from: ${req.body.name}</p> <p>${req.body.email}</p>`,
   };
-
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) console.log(err);
-    else console.log(info);
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+    res.status(200).json(req.body);
   });
-  res.status(200).json(req.body);
 }
