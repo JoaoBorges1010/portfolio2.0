676e59ae5151aaa270ffdc5721557da759dfd00b
joaoborges <joaoborgesbiz@gmail.com>
2023-06-04 10:16:53 +0100

    testing nodemailer if env not working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     host: "smtp.gmail.com",
     auth: {
       user: "emailsenderjoao@gmail.com",
-      pass: process.env.password,
+      pass: "ylfyhmgpkoihkfma",
     },
     secure: true,
   });
